0ae9fc0811206bd38d78068758e761aeb922a3f5
Hendrik Swanepoel <hendrik.swanepoel@gmail.com>
2016-08-27 17:51:31 +0200

    working with validation

--- src/components/EmployeeForm/index.js
+++ src/components/EmployeeForm/index.js
@@ -2,6 +2,20 @@ import React from 'react';
 import './styles.css';
 import { reduxForm } from 'redux-form';
 
+const validate = (values) => {
+  const errors = {};
+
+  if (!values.name) {
+    errors.name = 'Please provide a name';
+  }
+
+  if (!values.surname) {
+    errors.surname = 'Please provide a surname';
+  }
+
+  return errors;
+}
+
 const EmployeeForm = ({ addEmployee, fields: { name, surname}, handleSubmit }) => {
   return (
     <form
@@ -13,14 +27,18 @@ const EmployeeForm = ({ addEmployee, fields: { name, surname}, handleSubmit }) =
           type="text"
           placeholder="First Name"
           className="EmployeeForm-input"
+          {...name}
         />
+        {name.touched && name.error && <div className="EmployeeForm-error">{name.error}</div>}
       </div>
       <div>
         <input
           type="text"
           placeholder="Last Name"
           className="EmployeeForm-input"
+          {...surname}
         />
+        {surname.touched && surname.error && <div className="EmployeeForm-error">{surname.error}</div>}                  
       </div>
       <button
         type="submit"
@@ -35,4 +53,5 @@ const EmployeeForm = ({ addEmployee, fields: { name, surname}, handleSubmit }) =
 export default reduxForm({
   form: 'employee',
   fields: ['name', 'surname'],
+  validate,
 })(EmployeeForm);
